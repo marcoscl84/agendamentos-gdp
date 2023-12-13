02a4495092b4991e0ff0fb61c17d6ddd5cfd6163
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2023-12-13 12:15:12 -0300

    service appointment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const AppointmentService = require("./services/AppointmentService")
 
 app.use(express.static("public"));
 
@@ -17,11 +18,30 @@ mongoose.connect("mongodb://127.0.0.1:27017/agendamento").then(() => {
 })
 
 app.get("/", (req, res) => {
-    res.send("Oi!");
+    res.send("Opa!");
 });
 
 app.get("/cadastro", (req, res) => {
     res.render("create");
 });
+
+app.post("/create", async (req, res) => {
+
+    var status = await AppointmentService.Create(
+        req.body.name,
+        req.body.email,
+        req.body.description,
+        req.body.cpf,
+        req.body.date,
+        req.body.time
+    )
+
+    if(status){
+        res.redirect("/cadastro")
+    } else {
+        res.send("Ocorreu uma falha...")
+    }
+
+})
 
 app.listen(8080, () => {});
---
+++ services/AppointmentService.js
@@ -0,0 +1,31 @@
+const appointment = require("../models/AppointmentModel");
+const mongoose = require("mongoose");
+
+const Appo = mongoose.model("Appointment", appointment);
+
+class AppointmentService {
+
+    async Create(name, email, description, cpf, date, time){
+        var newAppo = new Appo({
+            name: name,
+            email: email,
+            description: description,
+            cpf: cpf,
+            date: date,
+            time: time,
+            finished: false
+        })
+
+        try {
+            await newAppo.save();    
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+        
+    }
+
+}
+
+module.exports = new AppointmentService();
